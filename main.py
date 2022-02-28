6318666dcccd377b4ae2c217804576285371341b
Alessandro <dixiedream88@gmail.com>
2022-02-28 09:04:01 +0100

    Need to change to C language

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import time
+from machine import I2C, Pin
 
-from machine import I2C
+# PINS DEFINITIONS
+LCD_SDA = 0
+LCD_SCL = 1
+TEMP_OUT = 4
+ROTARY_SW = 18
+ROTARY_DT = 17
+ROTARY_CLK = 16
+
+## LCD
 from lcd_api import LcdApi
 from pico_i2c_lcd import I2cLcd
 
@@ -8,28 +17,26 @@ I2C_ADDR     = 0x27
 I2C_NUM_ROWS = 4
 I2C_NUM_COLS = 20
 
-i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)
+i2c = I2C(0, sda=Pin(LCD_SDA), scl=Pin(LCD_SCL), freq=400000)
 lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
 
-lcd.clear()
-countdown = "{}"
-for i in reversed(range(1, 6)):
-    lcd.move_to(8, 0)
-    lcd.putstr(countdown.format(i))
-    time.sleep(1)
+## Temp sensor
+import dht
+
+tSensor = dht.DHT11(Pin(TEMP_OUT))
+
+def printTemp():
     lcd.clear()
-
-lcd.move_to(6,0)
-lcd.putstr("TVCT")
-time.sleep(5)
-lcd.clear()
-
-lcd.move_to(1,0)
-lcd.putstr("A domani")
-lcd.move_to(1, 1)
-lcd.putstr("amore mio")
-time.sleep(5)
-lcd.clear()
-
-
-time.sleep(5)
+    lcd.move_to(0, 0)
+    tempMsg = "Temp: {}C"
+    print(tSensor.temperature())
+    lcd.putstr(tempMsg.format(tSensor.temperature()))
+    lcd.move_to(0, 1)
+    humMsg = "Humidity: {}%"
+    print(tSensor.humidity())
+    lcd.putstr(humMsg.format(tSensor.humidity()))
+    
+# while True:
+    lcd.clear()
+    # printTemp()
+    # time.sleep(5)
