03b7f2ed59620963d60dbc348be4a44a6bf5bfb7
Alessandro <dixiedream88@gmail.com>
2022-03-01 13:49:49 +0100

    Clock working

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import time
 from machine import I2C, Pin
 
 # PINS DEFINITIONS
+CLOCK_SDA = 14
+CLOCK_SCL = 15
 LCD_SDA = 0
 LCD_SCL = 1
 TEMP_OUT = 4
@@ -22,8 +24,15 @@ lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
 
 ## Temp sensor
 import dht
-
 tSensor = dht.DHT11(Pin(TEMP_OUT))
+
+## Clock
+from ds3231_i2c import DS3231_I2C 
+ds_i2c = I2C(1, sda=Pin(CLOCK_SDA), scl=Pin(CLOCK_SCL))
+ds = DS3231_I2C(ds_i2c)
+
+# current_time = b'\x00\x06\x11\x01\x01\x03\x22' # sec min hour week day mon year
+# ds.set_time(current_time)
 
 def printTemp():
     tSensor.measure()
@@ -34,7 +43,17 @@ def printTemp():
     lcd.move_to(0, 1)
     humMsg = "Humidity: {}%"
     lcd.putstr(humMsg.format(tSensor.humidity()))
+
+def printTime():
+    t = ds.read_time()
+    lcd.clear()
+    lcd.move_to(0, 0)
+    lcd.putstr("Date: %02x/%02x/20%x" %(t[4],t[5],t[6]))
+    lcd.move_to(0, 1)
+    lcd.putstr("Time: %02x:%02x" %(t[2],t[1]))
     
 while True:
     printTemp()
+    time.sleep(5)
+    printTime()
     time.sleep(5)
